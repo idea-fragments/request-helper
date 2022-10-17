1edd51a55acd5c809aabca1c5c07976aeac462b3
Sam Inyang <saminyang09@gmail.com>
2022-10-17 12:36:20 -0400

    making improvements and fixes bugs as frontend is making use of this service

--- src/http/transformBodyToCamelCase.ts
+++ src/http/transformBodyToCamelCase.ts
@@ -1,5 +1,12 @@
 import camelCaseKeys from "camelcase-keys"
 
 export const transformBodyToCamelCase = <T>(body: T) => {
-  return camelCaseKeys<T>(body) as T
+  if(body instanceof Array) {
+    body.forEach((item, index) => {
+      body[index] = camelCaseKeys(item, {deep: true})
+    })
+    return body
+  }
+
+  return camelCaseKeys<T>(body, {deep: true}) as T
 }
